4affa79ffb3819c595b6810c3e44b4ab846f2b30
Yucky39 <create.to.music@gmail.com>
2019-06-15 21:21:43 +0900

    first commit

---
+++ Assets/Motor2ReverseControl.cs
@@ -0,0 +1,43 @@
+﻿// https://docs.unity3d.com/Manual/UnityWebRequest.html
+
+using UnityEngine;
+using System.Collections;
+// 5.4以降ではこちら
+using UnityEngine.Networking;
+
+public class Motor2ReverseControl : MonoBehaviour
+{
+    protected virtual void Start() => StartCoroutine(Text);
+
+    private IEnumerator Text
+    {
+        get
+        {
+            UnityWebRequest request = UnityWebRequest.Get("http://192.168.8.126:8080/api/rotate/motor2/rad/-0.2");
+            // 下記でも可
+            // UnityWebRequest request = new UnityWebRequest("http://example.com");
+            // methodプロパティにメソッドを渡すことで任意のメソッドを利用できるようになった
+            // request.method = UnityWebRequest.kHttpVerbGET;
+
+            // リクエスト送信
+            yield return request.Send();
+
+            // 通信エラーチェック
+            if (request.isNetworkError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                if (request.responseCode == 200)
+                {
+                    // UTF8文字列として取得する
+                    string text = request.downloadHandler.text;
+
+                    // バイナリデータとして取得する
+                    byte[] results = request.downloadHandler.data;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Motor3ReverseControl.cs
@@ -0,0 +1,44 @@
+﻿// https://docs.unity3d.com/Manual/UnityWebRequest.html
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Experimental.Networking;
+// 5.4以降ではこちら
+using UnityEngine.Networking;
+
+public class Motor3ReverseControl : MonoBehaviour
+    {
+    protected virtual void Start() => StartCoroutine(Text);
+
+    private IEnumerator Text
+    {
+        get
+        {
+            UnityWebRequest request = UnityWebRequest.Get("http://192.168.8.126:8080/api/rotate/motor3/rad/-0.1");
+            // 下記でも可
+            // UnityWebRequest request = new UnityWebRequest("http://example.com");
+            // methodプロパティにメソッドを渡すことで任意のメソッドを利用できるようになった
+            // request.method = UnityWebRequest.kHttpVerbGET;
+
+            // リクエスト送信
+            yield return request.Send();
+
+            // 通信エラーチェック
+            if (request.isNetworkError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                if (request.responseCode == 200)
+                {
+                    // UTF8文字列として取得する
+                    string text = request.downloadHandler.text;
+
+                    // バイナリデータとして取得する
+                    byte[] results = request.downloadHandler.data;
+                }
+            }
+        }
+    }
+}
